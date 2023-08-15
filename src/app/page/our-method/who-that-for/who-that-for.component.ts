a17d897f59792d2709356403b76cd2bb18afce13
mathieu <mathieu.perin.prog@gmail.com>
2023-08-15 11:14:50 +0200

    update who-that-for

--- src/app/page/our-method/who-that-for/who-that-for.component.ts
+++ src/app/page/our-method/who-that-for/who-that-for.component.ts
@@ -7,16 +7,12 @@ import {Component} from '@angular/core';
 })
 export class WhoThatForComponent
 {
-  statesOfMind: string[][] = [['ont soif de croissance et d’indépendance',
-    'veulent prendre un nouveau départ'],
-    ['veulent construire l’entreprise de leurs rêves'],
-    ['veulent vivrent correctement de leur passion',
-      'veulent retrouver une sérénité professionnelle'],
-    ['sont frustrés de ne pas pouvoir aider autant de monde qu’ils le souhaiteraient'],
-    ['veulent gagner en liberté',
-      'Veulent faire plus de ventes'],
-    ['ont besoin de se sentir guidés et soutenus dans le développement de leur activité'],
-    ['veulent apporter leur contribution au monde à leur manière',
-      'Souhaitent une méthode qui a fait ses preuves, à appliquer étape par étape']];
+  statesOfMind: string[][] = [
+    ['ont soif de croissance et d’indépendance', 'veulent prendre un nouveau départ'],
+    ['veulent construire l’entreprise de leurs rêves', 'veulent vivrent correctement de leur passion'],
+    ['veulent retrouver une sérénité professionnelle', 'sont frustrés de ne pas pouvoir aider autant de monde qu’ils le souhaiteraient'],
+    ['veulent gagner en liberté', 'Veulent faire plus de ventes'],
+    ['ont besoin de se sentir guidés et soutenus dans le développement de leur activité', 'veulent apporter leur contribution au monde à leur manière'],
+    ['Souhaitent une méthode qui a fait ses preuves, à appliquer étape par étape']];
 
 }
